4fa10d71202c1ae4b2cdb914e98ff7faac312bf0
Ruslan Temirali <rustemirgali@gmail.com>
2022-11-08 22:04:15 +0600

    fix: remove tests

--- src/test/java/demo/dpop/experiments/NativeDPoPExperimentsControllerIT.java
+++
@@ -1,9 +0,0 @@
-package demo.dpop.experiments;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeDPoPExperimentsControllerIT extends DPoPExperimentsControllerTest {
-
-    // Execute the same tests but in native mode.
-}
